372ddb23313def78946b773a73f58de0a296b313
tyfann <1342344773@qq.com>
2021-01-29 16:48:14 +0800

    version 1.0 add

---
+++ java_agent_user/src/main/java/com/agent/Main.java
@@ -0,0 +1,12 @@
+package com.agent;
+
+/**
+ * @author tyfann
+ * @date 2021/1/29 3:47 下午
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("main方法执行");
+        Thread.sleep(1000L);
+    }
+}
